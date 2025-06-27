de0030959f58913b175fcfd29f46d286b4c576d8
mazen <mazenmohamedsalah77@gmail.com>
2025-06-27 04:07:48 +0300

    Add project files.

---
+++ SurveyBasket.Api/Services/PollService.cs
@@ -0,0 +1,48 @@
+﻿
+namespace SurveyBasket.Api.Services;
+
+public class PollService : IPollService
+{
+    private static readonly List<Poll> _polls = [
+        new Poll
+        {
+            Id = 1,
+            Title = "Test1",
+            Description = "My First Poll",
+        }
+
+    ];
+
+    public IEnumerable<Poll> GetAll() => _polls;
+
+    public Poll? Get(int id) => _polls.SingleOrDefault(p => p.Id == id);
+
+    public Poll Add(Poll poll)
+    {
+        poll.Id = _polls.Count + 1;
+        _polls.Add(poll);
+        return poll;
+    }
+
+    public bool Update(int id, Poll poll)
+    {
+        var currentPoll = Get(id);
+
+        if (currentPoll is null)
+            return false;
+        currentPoll.Title = poll.Title;
+        currentPoll.Description = poll.Description;
+        
+        return true;
+    }
+
+    public bool Delete(int id)
+    {
+        var Poll=Get(id);
+
+        if(Poll is null)
+            return false;
+        _polls.Remove(Poll);
+        return true;
+    }
+}
